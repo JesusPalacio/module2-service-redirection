477854dc52a84a7e2d9edc7d0419e385153bf33a
JesusPalacio <jesusdanielpalacioavila50@gmail.com>
2025-11-28 15:54:49 -0500

    Rediseñando20

--- lambda/handler.js
+++ lambda/handler.js
@@ -38,21 +38,25 @@ module.exports.handler = async (event) => {
     const date = now.toISOString().split('T')[0]; // YYYY-MM-DD
     
     try {
+      const newEntry = [timestamp];
+
       await dynamo.update({
         TableName: TABLE_NAME,
         Key: { code: code },
-        UpdateExpression: "SET visits = if_not_exists(visits, :zero) + :inc, lastVisit = :timestamp",
+        UpdateExpression: "SET visits = if_not_exists(visits, :zero) + :inc, lastVisit = :timestamp, visit_history = list_append(if_not_exists(visit_history, :empty_list), :newEntry)",
         ExpressionAttributeValues: {
           ":inc": 1,
           ":zero": 0,
-          ":timestamp": timestamp
+          ":timestamp": timestamp,
+          ":newEntry": newEntry, 
+          ":empty_list": []  
         }
       }).promise();
       
       console.log(`Visita registrada para código: ${code} en fecha: ${date}`);
     } catch (updateErr) {
       console.error("Error actualizando visitas:", updateErr);
-    }
+    }
 
     return {
       statusCode: 302,
