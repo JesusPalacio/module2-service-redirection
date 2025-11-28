b91fd6e9afae6408ee141fa80e6c0ed3720e6afe
JesusPalacio <jesusdanielpalacioavila50@gmail.com>
2025-11-28 11:45:28 -0500

    Rediseñando11

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,21 +1,24 @@
+data "aws_api_gateway_rest_api" "existing" {
+  name = "redirect-service-api" 
+}
+
 resource "aws_api_gateway_resource" "redirect_path" {
-  rest_api_id = var.api_id
-  parent_id   = var.root_id
+  rest_api_id = data.aws_api_gateway_rest_api.existing.id
+  parent_id   = data.aws_api_gateway_rest_api.existing.root_resource_id
   path_part   = "{code}"
 }
 
 resource "aws_api_gateway_method" "get_method" {
-  rest_api_id   = var.api_id
+  rest_api_id   = data.aws_api_gateway_rest_api.existing.id
   resource_id   = aws_api_gateway_resource.redirect_path.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = var.api_id
-  resource_id = aws_api_gateway_resource.redirect_path.id
-  http_method = aws_api_gateway_method.get_method.http_method
-
+  rest_api_id             = data.aws_api_gateway_rest_api.existing.id
+  resource_id             = aws_api_gateway_resource.redirect_path.id
+  http_method             = aws_api_gateway_method.get_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.redirect_lambda.invoke_arn
@@ -26,5 +29,28 @@ resource "aws_lambda_permission" "allow_api" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.redirect_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "${var.api_execution_arn}/*/*"
+  
+  source_arn = "${data.aws_api_gateway_rest_api.existing.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = data.aws_api_gateway_rest_api.existing.id
+  
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.redirect_path.id,
+      aws_api_gateway_method.get_method.id,
+      aws_api_gateway_integration.lambda_integration.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = data.aws_api_gateway_rest_api.existing.id
+  stage_name    = "prod"
 }
